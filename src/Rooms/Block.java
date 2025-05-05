fbd171637cef710bcacd545156a84ade3e8e9fb6
matus008 <tuma2@spsejecna.cz>
2025-05-05 18:21:50 +0200

    Fungujuci graika blocku.

--- src/Rooms/Block.java
+++ src/Rooms/Block.java
@@ -7,19 +7,29 @@ public class Block {
     private BlockType type;
     private boolean mined;
 
-    private static final Image dirtImg = new ImageIcon("").getImage();
-    private static final Image coalImg = new ImageIcon("").getImage();
-    private static final Image goldImg = new ImageIcon("").getImage();
-    private static final Image diamondImg = new ImageIcon("").getImage();
-    private static final Image emptyImg = new ImageIcon("").getImage();
-    private static final Image silverImg = new ImageIcon("").getImage();
+    private static final Image dirtImg = new ImageIcon("src/Rooms/Textures/dirt.png").getImage();
+    private static final Image coalImg = new ImageIcon("src/Rooms/Textures/cole.png").getImage();
+    private static final Image goldImg = new ImageIcon("src/Rooms/Textures/gold.png").getImage();
+    private static final Image diamondImg = new ImageIcon("src/Rooms/Textures/diamond.png").getImage();
+    private static final Image emptyImg = new ImageIcon("src/Rooms/Textures/dirt.png").getImage();
+    private static final Image silverImg = new ImageIcon("src/Rooms/Textures/silver.png").getImage();
 
     public Block(BlockType type) {
         this.type = type;
         this.mined = false;
     }
 
-
+    public void draw(Graphics g, int x, int y, int tileSize) {
+        Image img = switch (type) {
+            case DIRT ->  dirtImg;
+            case COAL -> coalImg;
+            case GOLD -> goldImg;
+            case DIAMOND -> diamondImg;
+            case SILVER -> silverImg;
+            default -> emptyImg;
+        };
+        g.drawImage(img, x, y, tileSize, tileSize, null);
+    }
 
     public BlockType getType() {
         return type;
--- src/Rooms/MinePanel.java
+++ src/Rooms/MinePanel.java
@@ -1,26 +1,112 @@
 package Rooms;
+
 import Player.Player;
+import Valuables.*;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
 
 public class MinePanel extends JPanel implements KeyListener {
 
-    public MinePanel(Player p) {
+    private static final int BlockSize = 100;
+    private static final int MAP_WIDTH = 100;
+    private static final int MAP_HEIGHT = 100;
+
+    private Block[][] map;
+    private Player player;
+
+    // Player position on the map
+    private int playerX = 50;
+    private int playerY = 50;
+
+    public MinePanel(Player player) {
+        this.player = player;
+        this.map = new Block[MAP_WIDTH][MAP_HEIGHT];
+
+        generateMap();
+
+        setFocusable(true);
+        addKeyListener(this);
+    }
+
+    private void generateMap() {
+        for (int x = 0; x < MAP_WIDTH; x++) {
+            for (int y = 0; y < MAP_HEIGHT; y++) {
+                map[x][y] = BlockGenerator.generateRandomBlock();
+            }
+        }
     }
 
     @Override
-    public void keyTyped(KeyEvent e) {
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
 
+        int cameraSize = 8; // 8x8 tiles = 800x800px
+        int startX = playerX - cameraSize / 2;
+        int startY = playerY - cameraSize / 2;
+
+        for (int x = 0; x < cameraSize; x++) {
+            for (int y = 0; y < cameraSize; y++) {
+                int mapX = startX + x;
+                int mapY = startY + y;
+
+                if (mapX >= 0 && mapX < MAP_WIDTH && mapY >= 0 && mapY < MAP_HEIGHT) {
+                    map[mapX][mapY].draw(g, x * BlockSize, y * BlockSize, BlockSize);
+                }
+            }
+        }
+        for (int i = 0; i < map.length; i++) {
+
+        }
+
+        // Player rectangle (center)
+        g.setColor(Color.RED);
+        g.fillRect((cameraSize / 2) * BlockSize, (cameraSize / 2) * BlockSize, BlockSize, BlockSize);
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
-
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_W, KeyEvent.VK_UP -> move(0, -100);
+            case KeyEvent.VK_S, KeyEvent.VK_DOWN -> move(0, 100);
+            case KeyEvent.VK_A, KeyEvent.VK_LEFT -> move(-100, 0);
+            case KeyEvent.VK_D, KeyEvent.VK_RIGHT -> move(100, 0);
+            case KeyEvent.VK_SPACE -> mineBlock();
+        }
+        repaint();
     }
 
-    @Override
-    public void keyReleased(KeyEvent e) {
+    public void move(int dx, int dy) {
+        int newX = playerX + dx;
+        int newY = playerY + dy;
 
+        if (newX >= 0 && newX < MAP_WIDTH && newY >= 0 && newY < MAP_HEIGHT) {
+            playerX = newX;
+            playerY = newY;
+        }
     }
+
+    private void mineBlock() {
+        Block current = map[playerX][playerY];
+        if (!current.isMined()) {
+            current.mine();
+            BlockType type = current.getType();
+            if (type != BlockType.DIRT) {
+                try {
+                    Ores ore = switch (type) {
+                        case COAL -> new Cole();
+                        case SILVER -> new Silver();
+                        case GOLD -> new Gold();
+                        case DIAMOND -> new Diamond();
+                        default -> null;
+                    };
+                    if (ore != null) player.addOre(ore);
+                } catch (Exception ignored) {}
+            }
+        }
+    }
+
+    @Override public void keyTyped(KeyEvent e) {}
+    @Override public void keyReleased(KeyEvent e) {}
 }
